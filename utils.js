8be6eb531639970cb3da03e5120b257a13251ed3
Derrick Dominic <ddominic@umich.edu>
2016-07-22 09:06:29 -0400

    data tables (for filtering in table)

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 var add_row = function(type, text) {
     var encoded = encodeURIComponent(text)
-    $("#" + type + "s")
+    $("#" + type + "s tbody")
         .append("<tr><td><a href=/" + type + "?name=" + encoded +
                 ">" + text + "</a></td></tr>")
 };
